427012929173a6cc37ff52f42297463748897c8f
corentin-roussel <corentin.roussel@laplateforme.io>
2023-11-27 15:55:46 +0100

    job04: copy a string witha  src and destination, job05: duplicate a string with a source and malloc

---
+++ Jour01/Job04/my_strcpy.c
@@ -0,0 +1,32 @@
+//
+// Created by corentin on 27/11/23.
+//
+
+
+int length(char *str)
+{
+    int i = 0;
+    while(str[i] != 0)
+    {
+        i++;
+    }
+    return i;
+}
+
+char *my_strcopy(char *dest, const char *src)
+{
+    if(dest == NULL) return NULL;
+
+    char *return_ptr = dest;
+
+    while(*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+    *dest = '\0';
+
+    return return_ptr;
+}
