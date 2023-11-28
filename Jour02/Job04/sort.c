13b45c0f1316ac5c452f00f125195cd6e91bb7d0
corentin-roussel <corentin.roussel@laplateforme.io>
2023-11-28 19:33:45 +0100

    try to bubble sort an array of string in function of their ascii value

--- Jour02/Job04/sort.c
+++ Jour02/Job04/sort.c
@@ -1,3 +1,100 @@
 //
 // Created by corentin on 28/11/23.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void swap(char *a, char *b)
+{
+
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int my_strcmp(const char *s1, const char *s2)
+{
+    int i = 0;
+    int num =  0;
+
+    while(s1[i] != 0)
+    {
+        if(s2[i] != 0) {
+            if(s1[i] != s2[i])
+            {
+                num = (int)s1[i] - (int)s2[i];
+                if(num < 0)
+                {
+                    return num;
+                }else if (num > 0)
+                {
+                    return num;
+                }
+            }
+            i++;
+        }else
+        {
+            num = 1;
+            break;
+        }
+    }
+    if(s2[i] != 0)
+    {
+        num = -1;
+    }
+    return num;
+}
+
+
+char **sort(char **strs)
+{
+    int i = 0;
+
+    while(strs[i+1] != 0)
+    {
+        if(my_strcmp(strs[i], strs[i+1]) < 0)
+        {
+            char *temp = strs[i];
+            strs[i] = strs[i+1];
+            strs[i+1] = temp;
+            i++;
+        }else {
+            i++;
+        }
+
+    }
+    return strs;
+}
+
+int main(int argc, char **argv)
+{
+    char **grid = malloc(sizeof(char*) *6);
+
+    int i = 0;
+    while(i <9)
+    {
+        grid[i] = malloc(sizeof(char) *10);
+        i++;
+    }
+
+    grid[0] = "hello";
+    grid[1] = "world";
+    grid[2] = "to";
+    grid[3] = "are";
+    grid[4] = "bean";
+    grid[5] = 0;
+
+    char **sorted = sort(grid);
+
+    int j =0;
+
+    while(sorted[j] != 0)
+    {
+        printf("%s\n", sorted[j]);
+        j++;
+    }
+
+return 0;
+}
