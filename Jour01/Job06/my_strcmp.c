0c260a79bc72dfef6129a188e6fe2c3b156af45f
corentin-roussel <corentin.roussel@laplateforme.io>
2023-11-28 08:39:33 +0100

    comapre if a string is longer than the other

--- Jour01/Job06/my_strcmp.c
+++ Jour01/Job06/my_strcmp.c
@@ -2,6 +2,7 @@
 // Created by corentin on 27/11/23.
 //
 
+#include <stdio.h>
 
 int my_strcmp(const char *s1, const char *s2)
 {
@@ -10,18 +11,28 @@ int my_strcmp(const char *s1, const char *s2)
 
     while(s1[i] != 0)
     {
-        if(s1[i] != s2[i])
-        {
-            num = (int)s1[i] - (int)s2[i];
-            if(num < 0)
+        if(s2[i] != 0) {
+            if(s1[i] != s2[i])
             {
-                return num;
-            }else if (num > 0)
-            {
-                return num;
+                num = (int)s1[i] - (int)s2[i];
+                if(num < 0)
+                {
+                    return num;
+                }else if (num > 0)
+                {
+                    return num;
+                }
             }
+            i++;
+        }else
+        {
+            num = 1;
+            break;
         }
-        i++;
+    }
+    if(s2[i] != 0)
+    {
+        num = -1;
     }
     return num;
 }
