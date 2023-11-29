9a8472b7279da3b88b3abdbc7400211efd9d2ab5
corentin-roussel <corentin.roussel@laplateforme.io>
2023-11-29 19:30:43 +0100

    start of fibonnaci sequence need to work on it

--- Jour03/Job06/fibonacci.c
+++ Jour03/Job06/fibonacci.c
@@ -1,3 +1,34 @@
 //
 // Created by corentin on 29/11/23.
 //
+
+#include <unistd.h>
+#include <stdio.h>
+
+int fibonnaci(int num)
+{
+
+    int x,y,z;
+
+    while(1)
+    {
+        x = 0;
+        y = 1;
+
+        do {
+            printf("%d", x);
+
+            z = x + y;
+
+            x = y;
+
+            y = z;
+        } while (x < num);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int fib = fibonnaci(255);
+    return 0;
+}
