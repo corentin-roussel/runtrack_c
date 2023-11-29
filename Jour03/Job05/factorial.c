0a65df9b48c3e0b122a4fb75eca5a9e6e3424a51
corentin-roussel <corentin.roussel@laplateforme.io>
2023-11-29 21:46:08 +0100

    factorial with programm and my_putstr

--- Jour03/Job05/factorial.c
+++ Jour03/Job05/factorial.c
@@ -3,6 +3,7 @@
 //
 
 #include <unistd.h>
+#include <stdlib.h>
 
 int num_length(int num)
 {
@@ -13,6 +14,16 @@ int num_length(int num)
         i++;
     }
 
+    return i;
+}
+
+int length(const char *str)
+{
+    int i = 0;
+    while(str[i] != 0)
+    {
+        i++;
+    }
     return i;
 }
 
@@ -27,6 +38,76 @@ int my_atoi(char *str)
         ++i;
     }
     return ascii;
+}
+
+int *number_array(int num)
+{
+    int i =0;
+    int len = num_length(num);
+    int *numberArray = malloc(sizeof(int) *len);
+
+    while (i<len)
+    {
+        numberArray[i] = num % 10;
+        num /= 10;
+        i++;
+    }
+    numberArray[i] = '\0';
+    return numberArray;
+}
+
+char *reverse_string(char *str)
+{
+    int i = 0;
+    int len = length(str);
+    char *temp = malloc(sizeof(char));
+
+    while(i < len/2)
+    {
+        *temp = str[i];
+        str[i] = str[len-i-1];
+        str[len-i-1] = *temp;
+        i++;
+    }
+    free(temp);
+
+    return str;
+
+}
+
+char *my_itoa(int num)
+{
+    int i = 0;
+    int len = num_length(num);
+    int *arrayNum = number_array(num);
+    char *str = malloc(sizeof(char) *len+1);
+
+    while(i != len)
+    {
+        str[i] = arrayNum[i] + '0';
+        i++;
+    }
+    str[i] = '\0';
+
+    reverse_string(str);
+
+    return str;
+}
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void my_putstr(char *str)
+{
+    int i = 0;
+
+    while(str[i] != 0)
+    {
+        my_putchar(str[i]);
+        i++;
+    }
 }
 
 int factorial(int num)
@@ -50,10 +131,9 @@ int main(int argc, char **argv)
     int num = my_atoi(argv[1]);
 
     int fac = factorial(num);
-    //char write_fac = my_itoa(fac);
+    char *write_fac = my_itoa(fac);
 
-    int fac_size = num_length(fac);
 
-    write(1, &fac, fac_size);
+    my_putstr(write_fac);
     return 0;
 }
